83659bff67d80b4cdb553855a8bb095eb7151f00
oscarcordoba <oscar@avria.co>
2019-01-13 14:40:22 -0600

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import { ResponseBuilder } from './ResponseBuilder';
 
-
-console.log(ResponseBuilder.ok({hello: "hello"}, (error, response) => { console.log(error, response) } ));
-
 export default ResponseBuilder;
