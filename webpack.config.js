5fe73caf8ea573959df502d5c678c57292433ba3
oscarcordoba <oscar@avria.co>
2019-01-13 15:05:54 -0600

    Setup Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   mode:'development',
+  library: 'lambda-proxy-responses',
+  libraryTarget: 'commonjs',
   module: {
     rules: [
       {
