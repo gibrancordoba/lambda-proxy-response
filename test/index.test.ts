4c0103f8fc30fdb9bc7386a583b8b734c04ac4b0
oscarcordoba <oscar@avria.co>
2019-01-13 14:04:44 -0600

    Setup Package

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
 import ResponseBuilder from '../src/index';
 
 test('ResponseBuilder', () => {
+  console.log(ResponseBuilder.ok({hello: "hello"}, (error, response) => { console.log(error, response) } ));
   expect(true).toBe(true);
 });
