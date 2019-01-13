6bc3dab178f1e82d92b42ee9e62e6191c5279131
oscarcordoba <oscar@avria.co>
2019-01-13 13:59:49 -0600

    Setup Package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,63 @@
-import ResponseBuilder from './utils/ResponseBuilder';
-// tslint:disable-next-line:no-console
-console.log(ResponseBuilder);
-export default ResponseBuilder;
+import { ApiCallback, ApiResponse, IErrorResponseBody } from './interfaces/api-interfaces';
+import { ServerErrorStatus } from './interfaces/http-status-code-numbers';
+import {
+  // BadRequestException,
+  ErrorResult,
+  // ForbiddenException,
+  // InternalServerErrorException,
+  // NotFoundException,
+  ServerErrorException,
+} from './utils/errors';
+import { HttpStatusCode } from './utils/http-status-codes';
+
+/**
+ * Contains helper methods to generate a HTTP response.
+ */
+export default class ResponseBuilder {
+  // public static badRequest(code: string, description: string, callback: ApiCallback): void {
+  //   const errorResult: BadRequestResult = new BadRequestResult(code, description);
+  //   ResponseBuilder._returnAs<BadRequestResult>(errorResult, HttpStatusCode.BadRequest, callback);
+  // }
+
+  // public static forbidden(code: string, description: string, callback: ApiCallback): void {
+  //   const errorResult: ForbiddenResult = new ForbiddenResult(code, description);
+  //   ResponseBuilder._returnAs<ForbiddenResult>(errorResult, HttpStatusCode.Forbidden, callback);
+  // }
+
+  public static serverError(code: ServerErrorStatus, error: Error, callback: ApiCallback): void {
+    const errorResult: ServerErrorException = new ServerErrorException(code, 'Internal Error...');
+    ResponseBuilder._returnAs<ServerErrorException>(errorResult, HttpStatusCode.INTERNAL_SERVER_ERROR, callback);
+  }
+
+  // public static internalServerError(error: Error, callback: ApiCallback): void {
+  //   const errorResult: InternalServerErrorResult = new InternalServerErrorResult(ErrorCode.GeneralError, 'Sorry...');
+  //   ResponseBuilder._returnAs<InternalServerErrorResult>(errorResult, HttpStatusCode.InternalServerError, callback);
+  // }
+
+  // public static notFound(code: string, description: string, callback: ApiCallback): void {
+  //   const errorResult: NotFoundResult = new NotFoundResult(code, description);
+  //   ResponseBuilder._returnAs<NotFoundResult>(errorResult, HttpStatusCode.NotFound, callback);
+  // }
+
+  public static ok<T>(result: T, callback: ApiCallback): void {
+    // tslint:disable-next-line:no-console
+    console.log('OK method', result, callback);
+    ResponseBuilder._returnAs<T>(result, HttpStatusCode.OK, callback);
+  }
+
+  private static _returnAs<T>(result: T, statusCode: number, callback: ApiCallback): void {
+    const bodyObject: IErrorResponseBody | T =
+      result instanceof ErrorResult ? { statusCode: result.code, error: result } : result;
+    const response: ApiResponse = {
+      body: JSON.stringify(bodyObject),
+      headers: {
+        'Access-Control-Allow-Origin': '*', // This is required to make CORS work with AWS API Gateway Proxy Integration.
+      },
+      statusCode,
+      // tslint:disable-next-line:object-literal-sort-keys
+      isBase64Encoded: false,
+    };
+
+    callback(undefined, response);
+  }
+}
--- src/interfaces/api-interfaces.ts
+++ src/interfaces/api-interfaces.ts
@@ -1,6 +1,6 @@
 // tslint:disable-line no-implicit-dependencies (Using only the type information from the @types package.)
 import { APIGatewayEvent, Context, ProxyCallback, ProxyResult } from 'aws-lambda';
-import { ErrorResult } from '../error/errors';
+import { ErrorResult } from '../utils/errors';
 
 // Type aliases to hide the 'aws-lambda' package and have consistent, short naming.
 export type ApiCallback = ProxyCallback;
